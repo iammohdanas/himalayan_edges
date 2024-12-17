ac9a7d75b1b878f3129f55df4a2e28e5e52dd81c
mohdanascsc <mohd.anas@csc.gov.in>
2024-12-18 00:43:36 +0530

    changes to send aws for prod

--- kashmirguide/urls.py
+++ kashmirguide/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.conf import settings
 from mainapp import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -44,3 +45,6 @@ urlpatterns = [
     path('photo-gallery/', views.photo_gallery, name='photo_gallery'),
     path('', include('authenticator.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
