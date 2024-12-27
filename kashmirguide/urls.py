1471ec9e2cedc9e50efc36224afc29e28353502f
mohdanascsc <mohd.anas@csc.gov.in>
2024-12-28 02:06:49 +0530

    a

--- kashmirguide/urls.py
+++ kashmirguide/urls.py
@@ -45,3 +45,7 @@ urlpatterns = [
     path('photo-gallery/', views.photo_gallery, name='photo_gallery'),
     path('', include('authenticator.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
